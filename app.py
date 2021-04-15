af45de88422c874f3395f1f3ae746368e651f481
Chetan-Sagathiya <chetan.sagathiya018@gmail.com>
2021-04-15 14:34:49 +0530

    creating web site

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
-
+from keras.models import load_model
 app = Flask(__name__)
+
+model = load_model('model_inception.h5')
 
 @app.route('/')
 def home():
@@ -8,3 +10,14 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        print("method executed............", file)
+    return "best in the world"
+
+
+# keras used == 2.4.3
+# tensorflow used 2.4.1
