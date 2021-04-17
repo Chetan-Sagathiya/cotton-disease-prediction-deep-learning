a816398c4f042805aabab9e8e001d173c0f8da75
Chetan-Sagathiya <chetan.sagathiya018@gmail.com>
2021-04-17 17:35:51 +0530

    webpage prediction created

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, render_template, request
 from keras.models import load_model
+from keras.preprocessing.image import load_img, img_to_array
+from PIL import Image
+import os
+import numpy as np
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
 
 model = load_model('model_inception.h5')
@@ -15,9 +21,18 @@ if __name__ == '__main__':
 def upload():
     if request.method == 'POST':
         file = request.files['file']
-        print("method executed............", file)
-    return "best in the world"
+        print("method executed now............", file.__dict__)
 
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'img_uploads', secure_filename(file.filename))
+        file.save(file_path)
 
-# keras used == 2.4.3
-# tensorflow used 2.4.1
+        image = load_img(file_path, target_size=(224, 224))
+        image_arr = img_to_array(image)
+        image_arr = image_arr/255
+        x = np.expand_dims(image_arr, axis=0)
+        pred = model.predict(x)
+        pred = np.argmax(pred, axis=1)
+        print("********************************", pred)
+    return "result,"
